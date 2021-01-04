8f1d678816c4172cd21abc9604d160acfd04c1e0
Alexandra <alexandra.waelti@bluewin.ch>
2021-01-04 10:15:06 +0100

    Added bootstrap in VS Code (with npm)

--- src/ActualWeather.js
+++ src/ActualWeather.js
@@ -10,18 +10,20 @@ export default function ActualWeather() {
   };
   return (
     <div className="ActualWeather">
-      <div className="overview-date">
-        <h5 className="date_title">
-          <strong>Actual weather</strong>
-        </h5>
-        <img src={weatherData.icon} alt="icon" />
-        <h4 className="temperature">
-          <span id="temp-1">{weatherData.temp}°C</span>
-        </h4>
-        <ul>
-          <li className="wind">Wind: {weatherData.wind} km/h</li>
-          <li className="humidity">Humidity: {weatherData.humidtiy}%</li>
-        </ul>
+      <div className="container">
+        <div className="overview-date">
+          <h5 className="date_title">
+            <strong>Actual weather</strong>
+          </h5>
+          <img src={weatherData.icon} alt="icon" />
+          <h4 className="temperature">
+            <span id="temp-1">{weatherData.temp}°C</span>
+          </h4>
+          <ul>
+            <li className="wind">Wind: {weatherData.wind} km/h</li>
+            <li className="humidity">Humidity: {weatherData.humidtiy}%</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
